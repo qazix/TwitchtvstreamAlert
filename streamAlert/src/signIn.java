2ce576fdabc4fc351991962661d4f1c2243b42f9
Aaron <Aaron.Hanich@gmail.com>
2014-03-14 23:12:39 -0600

    Now gets the username from twitch
    It's uncommented but I'll get to it

--- streamAlert/src/signIn.java
+++ streamAlert/src/signIn.java
@@ -1,4 +1,6 @@
+
 import java.io.IOException;
+import java.net.URL;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -23,17 +25,19 @@ public class signIn extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String requestURI = request.getRequestURI();
+		String requestURI = request.getScheme() + "://" + request.getServerName() + ":" + 
+							request.getServerPort() + request.getRequestURI();
+//		System.out.println(requestURI);
 		int lastSlashIndex = requestURI.lastIndexOf("/");
 		
 		String cbURL = requestURI.substring(0, lastSlashIndex) + "/callback";
 		String clientID = "jnu1pncqiy1terszj189ejzjomd911r";
 		
-		String twitchAuthURL = "https://api.twitch.tv/kraken/oauth2/authorize?response_type=code&client_id=" +
-								clientID + "&redirect_uri=http://localhost:8080" + cbURL;
+		URL twitchAuthURL = new URL("https://api.twitch.tv/kraken/oauth2/authorize?response_type=code&client_id=" +
+								clientID + "&redirect_uri=" + cbURL + "&scope=user_read");
 		
 //		response.getWriter().print(twitchAuthURL);
-		response.sendRedirect(twitchAuthURL);
+		response.sendRedirect(twitchAuthURL.toString());
 	}
 
 	/**
